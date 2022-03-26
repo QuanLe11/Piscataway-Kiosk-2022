b342083867e41723329f696a76fb260263226f0b
Ian Zheng <izheng@terpmail.umd.edu>
2022-03-26 18:03:26 -0400

    Server and gitignore

---
+++ server.js
@@ -0,0 +1,18 @@
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
